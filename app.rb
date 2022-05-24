2629b3d2b1a8f5f9d7aaa142fc538c875e5704cc
Julien KARMANN <jkmn@protonmail.com>
2022-05-24 11:26:31 +0200

    tweets collection is now working, next step is creating the builder/butler in charge of constructing objects and sending them to a 3rd party microservice

--- app.rb
+++ app.rb
@@ -1,6 +1,42 @@
-# This is the base file for the app, where it will be launched from. 
+require_relative 'services/fletcher'
+
+# This is the base file for the app, where it will be launched from.
 
 # Since it's a microservices architecture, I'll call every element here
 
 # 1. Call fletcher, the service in charge of collecting tweets
 
+puts "Calling a Fletcher instance"
+fletcher = Fletcher.new
+puts "Fletcher Instance was created and is now running"
+
+puts "Test running"
+puts "------------------------------------"
+puts "defining the end and start time for the request"
+
+# timeframe until which the fetch is set for
+
+raw_end_time = Time.now - 20
+
+end_time = raw_end_time.utc.iso8601
+
+# timeframe from where the start the fetch (-1 hour, ergo 3600 (+ 20 seconds for the interval), from end_date)
+start_time = (raw_end_time - 3620).utc.iso8601
+
+puts "defining the params for the request"
+
+query = "#concours OR #jeuconcours"
+
+query_params = {
+  "query": query,
+  "max_results": 75,
+  "start_time": start_time,
+  "end_time": end_time,
+  "tweet.fields": "attachments,author_id,conversation_id,created_at,entities,id,lang"
+}
+
+puts "calling for fetch function of Fletcher"
+results = fletcher.fetch(query_params)
+puts "fletcher function was called"
+puts results.code, JSON.pretty_generate(JSON.parse(results.body))
+puts "results were sent and displayed"
--- services/fletcher.rb
+++ services/fletcher.rb
@@ -1,25 +1,50 @@
 # This is Fletcher, the service in charge of fetching tweets using the Twitter V2 API.
 # I'll make it a class
+require_relative '../api_keys'
 require 'rest-client'
+require 'json'
+require 'typhoeus'
+require 'date'
+require 'time'
 
 class Fletcher
   def initialize
     @api_key = API_KEY
     @api_secret_key = API_SECRET_KEY
     @bearer_token = BEARER_TOKEN
+    @interval = 3600
+    @search_url = "https://api.twitter.com/2/tweets/search/recent"
   end
+
+  # calculating start_time and end_time so that it does not overlap with previous call
+  # I'll make a call once an hour so it end_time must be the second the call is made
+  # API asks for a date formatted as "2020-07-02T18:00:00Z"
+
+  # interval is a variable so that it allows to be modified later
+  # so far set at 3600, being the number of seconds in an hour (60 * 60)
+
+
+
 
 # this is the query that's being sent to the Twitter API
 
-  def fetch(keywords, query)
+  def fetch(query_params)
 
-    query = "#concours OR #jeuconcours"
-    query_params = {
-      "query": query,
-      "max_results": 75,
-      "start_time":
+    options = {
+      method: 'get',
+      headers: {
+        "User-Agent": "WinWinBot",
+        "Authorization": "Bearer #{@bearer_token}"
+      },
+      params: query_params
     }
-    # 1. make the API call
-    RestClient.get()
+
+    request = Typhoeus::Request.new(@search_url, options)
+    response = request.run
+
+    return response
+
   end
+
+
 end
